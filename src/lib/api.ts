2b209ba01e6cb33ea2bca416fcfece3b0cea98ed
Marco Duarte <marcoaurelio_db@hotmail.com>
2026-07-14 11:29:49 -0300

    feat: update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://mostraai-api.com.br",
-  headers: { "Content-Type": "application/json" },
+  headers: { 
+    "Content-Type": "application/json", 
+    "ngrok-skip-browser-warning": "true" // Apenas para ambiente dev com ngrok
+  },
   timeout: 15000,
 });
