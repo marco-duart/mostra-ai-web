a2f160859ef4fed68dca11557c9eef669af92cd4
Marco Duarte <marcoaurelio_db@hotmail.com>
2026-07-18 21:50:14 -0300

    feat: add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import { StorePage } from "./pages/app/customer/StorePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { PrivacyPage } from "./pages/PrivacyPage";
 import { CancellationPage } from "./pages/CancellationPage";
+import { ProducerLoginPage } from "./pages/app/producer/ProducerLoginPage";
+import { ProducerDashboardPage } from "./pages/app/producer/ProducerDashboardPage";
+import { ProducerStorePage } from "./pages/app/producer/ProducerStorePage";
+import { ProducerProductsPage } from "./pages/app/producer/ProducerProductsPage";
+import { ProducerCategoriesPage } from "./pages/app/producer/ProducerCategoriesPage";
+import { ProducerLocationsPage } from "./pages/app/producer/ProducerLocationsPage";
+import { ProducerProtectedRoute } from "./components/producer/ProducerProtectedRoute";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<NotFoundPage />} />
+      <Route path="/producer/login" element={<ProducerLoginPage />} />
+      <Route path="/producer" element={<ProducerProtectedRoute />}>
+        <Route index element={<ProducerDashboardPage />} />
+        <Route path="store" element={<ProducerStorePage />} />
+        <Route path="products" element={<ProducerProductsPage />} />
+        <Route path="categories" element={<ProducerCategoriesPage />} />
+        <Route path="locations" element={<ProducerLocationsPage />} />
+      </Route>
       <Route path="/privacy" element={<PrivacyPage />} />
       <Route path="/cancel" element={<CancellationPage />} />
       <Route path="/:slug" element={<StorePage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { App } from "./App";
 import { globalStyles } from "./theme/stitches.config";
+import { queryClient } from "./lib/queryClient";
+import { ProducerAuthProvider } from "./contexts/producerAuth";
 
 globalStyles();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ProducerAuthProvider>
+        <BrowserRouter>
+          <App />
+          <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
+        </BrowserRouter>
+      </ProducerAuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
